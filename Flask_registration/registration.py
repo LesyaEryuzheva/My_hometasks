49acfc9915af63df58eefe4c750d0f800a0c3a01
LesyaEryuzheva <lesya-m-333@mail.ru>
2022-05-12 10:35:58 +0300

    Task 16

--- Flask_registration/registration.py
+++ Flask_registration/registration.py
@@ -2,44 +2,63 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
-registration = Flask(__name__)
-registration.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guests.db'
-registration.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(registration)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guest.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-class Persone(db.Model):
+person_event = db.Table('person_event',
+    db.Column('id_person', db.Integer, db.ForeignKey('persons.id')),
+    db.Column('id_event', db.Integer, db.ForeignKey('events.id'))
+)
+
+
+class Person(db.Model):
+    __tablename__ = 'persons'
     id = db.Column(db.Integer, primary_key=True)
     surname = db.Column(db.String(20), nullable=False)
-    name = db.Column(db.String(15), nullable=True)
     age = db.Column(db.Integer, nullable=False)
     phone_number = db.Column(db.Integer, nullable=False)
+    persons = db.relationship('Event', secondary=person_event, backref=db.backref('persons'))
 
     def __repr__(self):
-        return f'<Persone {self.id}>'
+        return f'<Person {self.surname}>'
 
 
-@registration.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == "POST":
-        surname = request.form['surname']
-        name = request.form['name']
-        age = request.form['age']
-        phone_number = request.form['phone_number']
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    month = db.Column(db.String(20), nullable=False)
 
-        persone = Persone(surname=surname, name=name, age=age, phone_number=phone_number)
-
-        try:
-            db.session.add(persone)
-            db.session.commit()
-            return redirect('/')
-        except Exception as exc:
-            return f'Произошла ошибка: {exc}'
-
-    else:
-        return render_template('index.html')
+    def __repr__(self):
+        return f'<Event {self.name}>'
 
 
 if __name__ == "__main__":
-    registration.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
+
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'Person': Person, 'Event': Event, 'person_event': person_event, create_persons: 'create_persons'}
+#
+# def create_persons():
+#     guest1 = Person(surname='Иванов', age='23', phone_number='123456')
+#     guest2 = Person(surname='Сидоров', age='30', phone_number='654321')
+#     guest3 = Person(surname='Петров', age='18', phone_number='789654')
+#     guest4 = Person(surname='Смирнов', age='33', phone_number='456987')
+#     event1 = Event(name='Выставка', month='Август')
+#     event2 = Event(name='Концерт', month='Июль')
+#     event3 = Event(name='Салют', month='Июнь')
+#     guest1.persons.append(event1, event2)
+#     guest2.persons.append(event3)
+#     guest3.persons.append(event1, event2, event3)
+#     guest4.persons.append(event2, event3)
+#
+#     db.session.add_all(guest1, guest2, guest3, guest4, event1, event2, event3)
+#     db.session.commit()
+
 
