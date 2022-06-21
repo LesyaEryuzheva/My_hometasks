ee9a63e52886f8aa947835842c8f86b64e752dff
LesyaEryuzheva <lesya-m-333@mail.ru>
2022-06-21 23:49:49 +0300

    corrected clothing store

--- Task_21/shop/clothing_store/models.py
+++ Task_21/shop/clothing_store/models.py
@@ -19,12 +19,6 @@ class Clothing(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='clothing')
     name = models.CharField(max_length=50, db_index=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('name',)
@@ -32,3 +26,15 @@ class Clothing(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Item(models.Model):
+    clothing = models.ForeignKey('Clothing', on_delete=models.CASCADE, related_name='items')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Clothing: {self.clothing.name}. Price: {self.price}'
--- Task_21/shop/clothing_store/views.py
+++ Task_21/shop/clothing_store/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, response, status, decorators
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
@@ -14,6 +14,23 @@ class CourseEnrollView(APIView):
 class ClothingListView(generics.ListAPIView):
     queryset = Clothing.objects.all()
     serializer_class = ClothingListSerializer
+
+    @decorators.action(
+        detail=True,
+        methods=['post'],
+        url_path='add-to-cart',
+    )
+    def add_item_to_cart(self, request, pk=None):
+        item = self.get_object()
+        cart = request.user.cart
+        cart.clothing.add(item)
+        return response.Response(status=status.HTTP_200_OK)
+
+    def delete_item_to_cart(self, request, pk=None):
+        item = self.get_object()
+        cart = request.user.cart
+        cart.clothing.delete(item)
+        return response.Response(status=status.HTTP_200_OK)
 
 
 class ClothingDetailView(generics.RetrieveUpdateDestroyAPIView):
